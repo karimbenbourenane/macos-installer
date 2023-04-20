9ecd0435642c6a269c124bdbef0ba3aef8c8d1f2
Karim Benbourenane <karim@benbourenane.com>
2023-04-19 23:28:26 -0400

    Get rid of directives, using shellcheckrc instead

--- install.sh
+++ install.sh
@@ -14,9 +14,7 @@ if [ -d '/opt/homebrew/share' ]; then
 fi
 
 # Load zsh startup files pulled in by chezmoi
-# shellcheck source=/Users/karim/.config/zsh/.zshenv
 . "$HOME/.config/zsh/.zshenv"
-# shellcheck source=/Users/karim/.config/zsh/.zshrc
 . "$HOME/.config/zsh/.zshrc"
 
 # Install all vim-plug plugins
