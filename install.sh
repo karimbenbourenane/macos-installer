9774013a14354eb6ed13c65d6acfb839c90462a4
Karim Benbourenane <karim@benbourenane.com>
2023-04-19 21:57:51 -0400

    Reorder statements and full path bins

--- install.sh
+++ install.sh
@@ -1,20 +1,16 @@
 #!/bin/sh
 
-# Install Oh My Zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-# Install chezmoi
-sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$(mktemp -d)" init karimbenbourenane --ssh --apply
-
-# Install Homebrew
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Install chezmoi, Oh My Zsh, and Homebrew
+/bin/sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$(mktemp -d)" init karimbenbourenane --ssh --apply
+/bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
 # Install bundles (system packages and desktop applications)
 brew bundle --file "$HOME/.config/brew/Brewfile" install
 
 # Fix permissions for some Homebrew packages on Apple Silicon
 if [ -d '/opt/homebrew/share' ]; then
-  chmod -R go-w '/opt/homebrew/share'
+  /bin/chmod -R go-w '/opt/homebrew/share'
 fi
 
 # Load zsh startup files pulled in by chezmoi
@@ -22,4 +18,4 @@ fi
 . "$HOME/.config/zsh/.zshrc"
 
 # Install all vim-plug plugins
-vi -E -s -u "$HOME/.vim/vimrc" +quitall
+/usr/bin/vi -E -s -u "$HOME/.vim/vimrc" +quitall
